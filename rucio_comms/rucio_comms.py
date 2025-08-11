776224f7c93df002ac5ec0d76c384ff12041a5c9
Maxim Potekhin <potekhin@bnl.gov>
2025-08-11 09:42:19 -0400

    Updated rucio_comms

--- rucio_comms/rucio_comms.py
+++ rucio_comms/rucio_comms.py
@@ -1,4 +1,18 @@
-from rucio.client import Client
+from rucio.client import Client as RucioClient
 from rucio.client.uploadclient import UploadClient
 from rucio.common.exception import RucioException
 
+from typing import Optional, List, Dict, Any
+
+class RucioOrchestrator:
+    """
+    A class to orchestrate Rucio operations for data management.    
+    This class coordinates the following steps:
+    1. Create an empty and OPEN rucio dataset
+    2. Register files with existing PFNs 
+    3. Add these files to the dataset
+    4. Close the dataset
+    """
+    
+    def __init__(self, rucio_client: Optional[RucioClient] = None):
+        pass
